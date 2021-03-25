08ade03ce9371f320824765cb0098ae36ab7477c
chakresh <chakresh@acis.io>
2021-03-25 23:58:27 +0530

    Junit tests

--- src/test/java/com/patientInfo/patientmgmt/PatientMgmtApplicationTests.java
+++ src/test/java/com/patientInfo/patientmgmt/PatientMgmtApplicationTests.java
@@ -1,13 +1,80 @@
 package com.patientInfo.patientmgmt;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.Assert.assertEquals;
 
-@SpringBootTest
-class PatientMgmtApplicationTests {
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.patientInfo.patientmgmt.dto.PatientDto;
+import com.patientInfo.patientmgmt.entity.PatientEntity;
+
+public class PatientMgmtApplicationTests extends PatientServiceTestConfiguration {
+
+	@Override
+	@Before
+	public void setUp() {
+		super.setUp();
+	}
 
 	@Test
-	void contextLoads() {
+	public void getProductsList() throws Exception {
+		String uri = "/patient";
+		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri).accept(MediaType.APPLICATION_JSON_VALUE))
+				.andReturn();
+
+		int status = mvcResult.getResponse().getStatus();
+		assertEquals(200, status);
+		String content = mvcResult.getResponse().getContentAsString();
+	}
+
+	@Test
+	public void createProduct() throws Exception {
+		String uri = "/patient";
+		PatientEntity patient = new PatientEntity();
+		patient.setId(6L);
+		patient.setName("Ginger");
+		String inputJson = super.mapToJson(patient);
+		MvcResult mvcResult = mvc.perform(
+				MockMvcRequestBuilders.post(uri).contentType(MediaType.APPLICATION_JSON_VALUE).content(inputJson))
+				.andReturn();
+
+		int status = mvcResult.getResponse().getStatus();
+		assertEquals(200, status);
+		String content = mvcResult.getResponse().getContentAsString();
+		ObjectMapper mapper = new ObjectMapper();
+		assertEquals(content, mapper.writeValueAsString(patient));
+	}
+
+	@Test
+	public void updateProduct() throws Exception {
+		String uri = "/patient";
+		PatientDto patient = new PatientDto();
+		patient.setId(1L);
+		patient.setName("Lemon");
+		String inputJson = super.mapToJson(patient);
+		MvcResult mvcResult = mvc.perform(
+				MockMvcRequestBuilders.put(uri).contentType(MediaType.APPLICATION_JSON_VALUE).content(inputJson))
+				.andReturn();
+
+		int status = mvcResult.getResponse().getStatus();
+		assertEquals(200, status);
+		String content = mvcResult.getResponse().getContentAsString();
+		ObjectMapper mapper = new ObjectMapper();
+		assertEquals(content, mapper.writeValueAsString(patient));
+	}
+
+	@Test
+	public void deleteProduct() throws Exception {
+		String uri = "/patient/1";
+		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.delete(uri)).andReturn();
+		int status = mvcResult.getResponse().getStatus();
+		assertEquals(200, status);
+		String content = mvcResult.getResponse().getContentAsString();
+		assertEquals(content, "");
 	}
 
 }
